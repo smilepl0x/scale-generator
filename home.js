9b7d561288f5acfd93c294d542cd1552329d8a6c
Danny Faught <dfaught@protonmail.com>
2020-07-28 17:46:17 -0500

    font changes

--- home.js
+++ home.js
@@ -358,7 +358,7 @@ $(document).ready(() => {
 
     // Display the directions and animate upon load
     animateDirections("Please select a Triad");
-    $("#banner").animate({height: "25%", fontSize: "4vw"})
+    $("#banner").animate({height: "25%", fontSize: "2em"})
     $("#display").fadeIn("slow");
 
     // Create triad buttons
