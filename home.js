64a9a376f33caeac288f68145504dd4b6e4c415c
Danny Faught <dfaught@protonmail.com>
2020-07-27 20:23:51 -0500

    fixed typo in mixolydian mode

--- home.js
+++ home.js
@@ -9,7 +9,7 @@ const scales = [
     {name: "Dorian", intervals: [0, 2, 3, 5, 7, 9, 10]},
     {name: "Phrygian", intervals: [0, 1, 3, 5, 7, 8, 10]},
     {name: "Lydian", intervals: [0, 2, 4, 6, 7, 9, 11]},
-    {name: "Mixolydian", intervals: [0, 2, 4, 5, 7, 9, 11]},
+    {name: "Mixolydian", intervals: [0, 2, 4, 5, 7, 9, 10]},
     {name: "Aeolian", intervals: [0, 2, 3, 5, 7, 8, 10]},
     {name: "Locrian", intervals: [0, 1, 3, 5, 6, 8, 10]},
 
